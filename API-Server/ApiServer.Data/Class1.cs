1a11b084981ab28636e0eea2d02c27cca8ebc4c4
Mingyang Zheng <zhengzmy@gmail.com>
2020-08-09 01:13:03 -0700

    API-Server initialization

---
+++ API-Server/ApiServer.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiServer.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ API-Server/ApiServer.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiServer.Services
+{
+    public class Class1
+    {
+    }
+}
