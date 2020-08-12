62663746546e2cc11fcbf52bdcd668c81094fbe5
Mingyang Zheng <zhengzmy@gmail.com>
2020-08-12 00:11:55 -0700

    ApiServer.Data public async Task<int> CommitAsync()
    UnitOfWork

---
+++ API-Server/ApiServer.Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using ApiServer.Core;
+using ApiServer.Core.Repositories;
+using ApiServer.Data.Repositories;
+
+namespace ApiServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApiServerDbContext _context;
+        private ProblemRepository _problemRepository;
+
+        public UnitOfWork(ApiServerDbContext context)
+        {
+            this._context = context;
+        }
+ 
+        public IProblemRepository ProblemRepository => _problemRepository ?? new ProblemRepository(_context);
+
+        public async Task<int> CommitAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
